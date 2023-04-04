94386ec7e954cf7b7277f0e6e020cef43f9e1352
aydachettit <dantetran04@gmail.com>
2023-04-04 10:02:26 +0700

    Nam

---
+++ Entity/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Author
+    {
+        
+        public int ID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required,MaxLength(50)]
+        public string? Publisher_country { get; set; }
+        public ICollection<String>? book_name { get; set; }
+    }
+}
