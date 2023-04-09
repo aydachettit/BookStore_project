23b3a9e176c3d7a52c537b14e5d0bef047f00cd4
aydachettit <dantetran04@gmail.com>
2023-04-09 08:01:10 +0700

    nam

---
+++ BookStore_project/Models/Author/AuthorCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore_project.Models.Author
+{
+    public class AuthorCreateViewModel
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string? Img_url { get; set; }
+    }
+}
---
+++ BookStore_project/Models/Author/AuthorDeleteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore_project.Models.Author
+{
+    public class AuthorDeleteViewModel
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string? img_url { get; set; }
+    }
+}
