0ed5b5d1e0f0028e80bf4e7da8002fe94da89f3f
DBZNes <97336178+DBZNes@users.noreply.github.com>
2023-04-12 17:32:28 +0700

    Add files via upload

---
+++ BookStore_project/Models/Status/StatusDeleteViewModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_project.Models.Status 
+{
+    public class StatusDeleteViewModel {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BookStore_project/Models/Status/StatusEditViewModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_project.Models.Status 
+{
+    public class StatusEditViewModel {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
