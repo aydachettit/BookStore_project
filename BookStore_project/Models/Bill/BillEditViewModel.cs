b5fffc7edac456b438e2a2e9e32a4e8506ec65c2
DBZNes <97336178+DBZNes@users.noreply.github.com>
2023-04-12 17:33:02 +0700

    Add files via upload

---
+++ BookStore_project/Models/Bill/BillEditViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_project.Models.Bill
+{
+    public class BillEditViewModel
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public int Total_money { get; set; }
+
+        public int Customer_ID { get; set; }
+        public int Employee_ID { get; set; }
+        public int Bill_status_ID { get; set;}
+    }
+}
