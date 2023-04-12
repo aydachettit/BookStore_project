aa362c15705417f9c5ec9fe3e31337b613a4c3ea
DBZNes <97336178+DBZNes@users.noreply.github.com>
2023-04-12 17:28:52 +0700

    Add files via upload

---
+++ Entity/Status.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity {
+    public class Status {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
