aa925496d47090cd427a4d9bfa30aebc83f40a0e
Duy Huỳnh <huynhlamduylcd@gmail.com>
2023-05-06 01:45:14 +0700

    lduy done chi tiet san pham

--- BookStore_project/Controllers/BookController.cs
+++ BookStore_project/Controllers/BookController.cs
@@ -432,9 +432,11 @@ namespace BookStore_project.Controllers
         //[HttpPost]
         //[ValidateAntiForgeryToken]
         [HttpGet]
-        public async Task<IActionResult> CreateProductDetail(int productID = 1, int authorID = 1)
+        public async Task<IActionResult> CreateProductDetail(int ID , int authorID = 1)
         {
-            var ProductDetail = _bookService.GetByID(productID);
+
+            //ViewData["CurrentFilter"] = prodIDuctID;
+            var ProductDetail = _bookService.GetByID(ID);
             var author = _authorService.GetById(authorID);
 
             if (author == null || ProductDetail == null)
