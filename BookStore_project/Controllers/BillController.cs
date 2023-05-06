f3000188910938030eaeb560fba2c398edd17d89
aydachettit <119464420+aydachettit@users.noreply.github.com>
2023-05-06 22:20:13 +0700

    Update BillController.cs

--- BookStore_project/Controllers/BillController.cs
+++ BookStore_project/Controllers/BillController.cs
@@ -20,9 +20,12 @@ namespace BookStore_project.Controllers {
             _StatusService = StatusService;
             _billService = billService;
         }
-        [Authorize(Roles = "Admin")]
+        
          public IActionResult Index(int ? page){
-           
+            if (!User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
             var model = _billService.GetAll().Select( Bill => new BillIndexViewModel
             {
                 ID = Bill.ID,
